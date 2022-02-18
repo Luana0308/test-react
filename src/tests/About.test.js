42b02f3219900a815a710f3dd628105b12a7097a
Luana <luanaff-2008@hotmail.com>
2022-02-18 19:19:00 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import { About } from '../components';
 
 describe('Teste se a página contém as informações sobre a Pokédex.', () => {
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
     renderWithRouter(<About />);
+
     const titleAbout = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
     expect(titleAbout).toBeInTheDocument();
   });
 
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
     renderWithRouter(<About />);
+
     const paragraph1 = screen.getByText(/This application simulates a Pokédex/);
     expect(paragraph1).toBeInTheDocument();
 
@@ -22,6 +23,7 @@ describe('Teste se a página contém as informações sobre a Pokédex.', () => 
 
   test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
+
     const image = screen.getByRole('img');
     expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
     expect(image).toHaveAttribute('alt', 'Pokédex');
