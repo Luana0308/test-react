12f0fc36303a7c650e069466d210c6b3351e44be
Luana <luanaff-2008@hotmail.com>
2022-02-18 15:27:39 -0300

    inicio requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('teste se no componente App possui as aplicações', () => {
+  test('Teste se a aplicação contém um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getAllByRole('link', { name: 'Home' });
+
+    expect(linkHome).toBeInTheDocument();
+  });
+});
