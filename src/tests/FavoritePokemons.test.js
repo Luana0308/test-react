dd70e22734592a752217500794c16bb541cc2810
Luana <luanaff-2008@hotmail.com>
2022-02-18 20:16:18 -0300

    inicio requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('teste as aplicações do Favorite Pokemons', () => {
+  test(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+     se a pessoa não tiver pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const NotFoundFavorites = screen.getByText('No favorite pokemon found');
+    expect(NotFoundFavorites).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkMoreDetails).toBeInTheDocument();
+    userEvent.click(linkMoreDetails);
+  });
+});
