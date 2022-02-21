6ce754da535ed179142774f6b6f572fe7c7376f2
Luana <luanaff-2008@hotmail.com>
2022-02-21 01:19:52 -0300

    inicio requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('O nome correto do Pokémon deve ser mostrado na tela;', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent('Pikachu');
+  });
+
+  test('O tipo correto do pokémon deve ser mostrado na tela.', () => {
+    renderWithRouter(<App />);
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent('Electric');
+  });
+});
